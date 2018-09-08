c0a12d169c0ade5b9f0a48f30ba7c9fe388b166e
jaswant singh <32160088+bishtjaswant@users.noreply.github.com>
2018-09-08 13:09:32 +0530

    install jwt token in laravel and create components and download bootstrap 4 in frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ResetRequestComponent } from './components/password/reset-request/reset-request.component';
+import { ResponseRequestComponent } from './components/password/response-request/response-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    ResetRequestComponent,
+    ResponseRequestComponent
   ],
   imports: [
     BrowserModule
